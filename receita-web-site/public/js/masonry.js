e62d3ea53d08f0206a9f8a887461d51f1c53de3c
IF <iisaquielfernandes@gmail.com>
2020-05-31 22:44:42 +0000

    Update masonry.js

--- receita-web-site/public/js/masonry.js
+++ receita-web-site/public/js/masonry.js
@@ -2,3 +2,7 @@ var elem = document.querySelector('.grid');
 var msnry = new Masonry( elem, {
   itemSelector: '.grid-item',
 });
+
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.grid-item', { delay: 500 });
+ScrollReveal().reveal('.punchline', { delay: 2000 });
