7ddb4be2496de7fba224ae91b70e3ac6147a99cb
IF <iisaquielfernandes@gmail.com>
2020-06-01 07:26:22 +0000

    Update Contacto.js

--- receita-web-site/components/Contacto.js
+++ receita-web-site/components/Contacto.js
@@ -8,8 +8,7 @@ const Contacto = () => (
             <aside className="address p-2">
             <h2 >Find Us</h2>
             <div className="row justify-content-sm-center">
-                <p className="col-sm-12 col-md-4 col-lg-3"><strong>Address</strong><br /><i className="fas fa-map-marker-alt" /> 123
-                Main Street<br /><i className="fas fa-map-marker-alt" /> New York, NY 10001</p>
+                <p className="col-sm-12 col-md-4 col-lg-3"><strong>Address</strong><br /><i className="fas fa-map-marker-alt" /> Setúbal, 2810-226 Almada<br /><i className="fas fa-map-marker-alt" /> Lisboa, Portugal</p>
                 <p className="col-sm-12 col-md-2 col-lg-3"><strong>Contact</strong><br /><i className="fas fa-mobile-alt mr-1" />
                  935728467<i className="fas
                 fa-phone-square-alt ml-2 mr-1" /> 945790354<br /><i className="fas
