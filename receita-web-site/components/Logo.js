c87974fd647aee48f32738ebd3407dd8d5090b14
IF <iisaquielfernandes@gmail.com>
2020-06-03 07:34:35 +0000

    Update Logo.js

--- receita-web-site/components/Logo.js
+++ receita-web-site/components/Logo.js
@@ -2,7 +2,7 @@ import Router from 'next/router';
 
 const Logo = () => {
     return (
-        <img src="img/logo/logo.jpeg" width={30} height={30} className="rounded mr-1 d-inline-block align-top" alt="logo" loading="lazy" />
+        <img src="/img/logo/logo.jpeg" width={30} height={30} className="rounded mr-1 d-inline-block align-top" alt="logo" loading="lazy" />
     )
 }
 
