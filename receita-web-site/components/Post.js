e28a846c8134983e75e43eaba4682442df04ef17
IF <iisaquielfernandes@gmail.com>
2020-06-04 07:03:37 +0000

    Update Post.js

--- receita-web-site/components/Post.js
+++ receita-web-site/components/Post.js
@@ -15,7 +15,7 @@ const  Post = ({ posts, loading }) => {
     return (
         <div className="card-columns clearfix grid" >
             { posts.map((p) => (
-                <article id={p.fields.categoria} className="card img-container grid-item" key={p.sys.id}>
+                <article id={p.fields.categoria} className="card rounded-0 img-container grid-item" key={p.sys.id}>
                     <img src={p.fields.image.fields.file.url} className="card-img-top img-fluid rounded-0" alt="receita-img" />
                     <button className="rounded-left btn-ver-receita" onClick={() => Router.push(`/post/[id]`, `/post/${p.sys.id}`)}>
                         Ver Receita <i className="fa fa-cutlery" aria-hidden="true"></i>
