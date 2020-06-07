527811279fdf71e50d885c22c49ebf8ec872e8a0
IF <iisaquielfernandes@gmail.com>
2020-06-07 13:26:19 +0000

    Update Navigation.js

--- receita-web-site/components/Navigation.js
+++ receita-web-site/components/Navigation.js
@@ -17,11 +17,11 @@ const Navigation = () => {
                     ArtGastronomic
                 </a>
             </Link>
-            <button className="navbar-toggler" type="button" data-toggle="offcanvas" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" onClick={(e) => setVisibleLeft(true)} data-toggle="oooffcanvas" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                  <span className="navbar-toggler-icon" />
             </button>
 
-            <div className="navbar-collapse offcanvas-collapse" id="navbarSupportedContent">
+            <div className="navbar-collapse oooffcanvas-collapse" id="navbarContent">
             
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
@@ -45,8 +45,34 @@ const Navigation = () => {
                     </Link>
                 </li>
             </ul>
-            
             </div>
+
+            <Sidebar visible={visibleRight} position="right" baseZIndex={1000000} onHide={() => setVisibleRight(false)}>
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <Link href="/index">
+                        <a className="nav-link pl-1">Home</a>
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/">
+                         <a className="nav-link pl-1" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank">YouTube</a>
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/">
+                        <a href="#receitas" className="nav-link pl-1">Receitas</a>
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/">
+                         <a href="#contact" className="nav-link pl-1">Contactos</a>
+                    </Link>
+                </li>
+              </ul>
+              <Button type="button" onClick={(e) => setVisibleRight(false)} label="Cancel" className="p-button-secondary"/>
+            </Sidebar>
+
         </nav>
     </header>
  )
