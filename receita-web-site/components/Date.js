3c8f2d5210f6674f160b07200d18facb65185deb
IF <iisaquielfernandes@gmail.com>
2020-06-07 17:58:17 +0000

    Update Date.js

--- receita-web-site/components/Date.js
+++ receita-web-site/components/Date.js
@@ -3,7 +3,7 @@ import { formatDistanceToNow, format } from 'date-fns';
 const Date = ({ dateString }) => {
     return (
         <time dateTime={dateString}>
-            {formatDistanceToNow(new Date(dataString))}
+            {formatDistanceToNow(dataString)}
         </time>
     )
 }
