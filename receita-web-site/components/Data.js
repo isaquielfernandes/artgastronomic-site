ff241baa05422424c6372f3c6700d0ee41843fee
IF <iisaquielfernandes@gmail.com>
2020-06-07 18:47:46 +0000

    Update and rename Date.js to Data.js

---
+++ receita-web-site/components/Data.js
@@ -0,0 +1,10 @@
+import { formatDistanceToNow, format } from 'date-fns';
+
+export default function Date({ dateString }) {
+  return (
+    <time dateTime={dateString}>
+      {format(new Date(dateString), 'LLLL	d, yyyy')}
+    </time>
+  )
+}
+
