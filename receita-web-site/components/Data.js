6a298c28d126866098191a08f5fd3f59fc5c281e
IF <iisaquielfernandes@gmail.com>
2020-06-07 18:59:41 +0000

    Update Data.js

--- receita-web-site/components/Data.js
+++ receita-web-site/components/Data.js
@@ -3,7 +3,7 @@ import { formatISO, formatDistanceToNow, format } from 'date-fns';
 export default function Date({ dateString }) {
   return (
     <time dateTime={dateString}>
-      {format(new Date(dateString), 'LLLL	d, yyyy')}
+      {format(dateString, 'LLLL	d, yyyy')}
     </time>
   )
 }
