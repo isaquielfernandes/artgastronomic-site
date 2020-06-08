b45843c7f8f50c5df092f0d185c9e21aa43f9b5e
IF <iisaquielfernandes@gmail.com>
2020-06-08 19:38:25 +0000

    Update Data.js

--- receita-web-site/components/Data.js
+++ receita-web-site/components/Data.js
@@ -1,10 +1,8 @@
-import { formatISO, formatDistanceToNow, format } from 'date-fns';
+import { parseISO, formatISO, formatDistanceToNow, format } from 'date-fns';
+
+import { , format } from 'date-fns'
 
 export default function Date({ dateString }) {
-  return (
-    <time dateTime={dateString}>
-      {formatISO(dateString, { representation: 'date' })}
-    </time>
-  )
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
 }
-
