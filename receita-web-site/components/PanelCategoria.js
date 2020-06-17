44416e559d8211bf2584c3402fdd701728ccce3a
IF <iisaquielfernandes@gmail.com>
2020-06-17 20:44:21 +0000

    Update PanelCategoria.js

--- receita-web-site/components/PanelCategoria.js
+++ receita-web-site/components/PanelCategoria.js
@@ -1,8 +1,8 @@
 import {Panel} from 'primereact/panel';
 
 const PanelCategoria = ({children}) => (
-    <div className="col-sm-12 col-md-12 col-lg-3 my-5">
-        <aside id="categoria-section" className="categoria-section d-none d-lg-block">    
+    <div className="col-sm-12 col-md-12 col-lg-3 my-5 d-none d-lg-block">
+        <aside id="categoria-section" className="categoria-section">    
             <Panel id="categoria-panel" header="Categorias" className="p-0 p-fluid">
                 {children}
             </Panel>   
