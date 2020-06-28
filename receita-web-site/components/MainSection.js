56f7466a380caf06e4e0b26b4ad5c28bca7ff22a
IF <iisaquielfernandes@gmail.com>
2020-06-28 21:56:55 +0000

    Update MainSection.js

--- receita-web-site/components/MainSection.js
+++ receita-web-site/components/MainSection.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react';
 const MainSection = (prosps) => {
 
     return(
-        <div role="main">
+        <main role="main">
             <hr className="hr mt-1" />
             <div id="receitas" className="container mt-4">
                 <div className="row">
                     {prosps.children}
                 </div>
             </div>
-        </div>
+        </main>
     )
 
 };
