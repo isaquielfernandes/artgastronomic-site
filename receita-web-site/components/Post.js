a0a7dc410ff60074494e740910f693587da60be8
IF <iisaquielfernandes@gmail.com>
2020-06-28 23:33:39 +0000

    Update Post.js

--- receita-web-site/components/Post.js
+++ receita-web-site/components/Post.js
@@ -18,9 +18,9 @@ const  Post = ({ posts, loading }) => {
                 <article key={p.sys.id} className="col mx-auto mb-3 px-sm-2">
                     <div id={p.fields.categoria} className="card rounded-0 h-100 img-container" >
                         <img src={p.fields.image.fields.file.url} className="card-img-top img-fluid rounded-0" alt={p.fields.nome}/>
-                        <button className="rounded-left btn-ver-receita" onClick={() => Router.push(`/post/[id]`, `/post/${p.sys.id}`)}>
+                        <Link className="rounded-left btn-ver-receita" href=$/post/[id]" as={`/post/${p.sys.id}`}>
                             Ver Receita <i className="fa fa-cutlery" aria-hidden="true"></i>
-                        </button>
+                        </Link>
                         <div className="card-body p-2">
                             <h5 className="font-weight-lighter text-truncate">{p.fields.nome}</h5>
                         </div>
