eb8479c9534262a36aeedb110982abf129b5e0e3
IF <iisaquielfernandes@gmail.com>
2020-06-28 21:22:27 +0000

    Update Post.js

--- receita-web-site/components/Post.js
+++ receita-web-site/components/Post.js
@@ -13,7 +13,7 @@ const  Post = ({ posts, loading }) => {
     }
    
     return (
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 clearfix">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 clearfix" itemtype ="http://schema.org/Receita">
             { posts.map((p) => (
                 <article key={p.sys.id} className="col mx-auto mb-3 px-sm-2">
                     <div id={p.fields.categoria} className="card rounded-0 h-100 img-container" >
