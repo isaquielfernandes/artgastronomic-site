c0d1e48cee5bd233a952f9fd48cfc0d1f90b66c8
IF <iisaquielfernandes@gmail.com>
2020-06-28 17:38:17 +0000

    Update Meta.js

--- receita-web-site/components/Meta.js
+++ receita-web-site/components/Meta.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 
 const Meta = () => (
     <Head>
-            <title>Art Gastronomic</title>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
             <meta name="description" content="Site de receitas"/>
