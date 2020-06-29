5031e9bc384a872695f203edab5a6aaaa42a17b1
IF <iisaquielfernandes@gmail.com>
2020-06-29 09:58:03 +0000

    Update [id].js

--- receita-web-site/pages/post/[id].js
+++ receita-web-site/pages/post/[id].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Link from 'next/link';
 import { client } from '../../lib/api';
 import ErrorPage from 'next/error';
 import Container from '../../components/Container';
