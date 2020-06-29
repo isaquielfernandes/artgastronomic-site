9f36b4cef3b757f637411f6a32d6c61023bd8619
IF <iisaquielfernandes@gmail.com>
2020-06-29 10:03:20 +0000

    Update [id].js

--- receita-web-site/pages/post/[id].js
+++ receita-web-site/pages/post/[id].js
@@ -34,6 +34,9 @@ const Receita = ( { receita } ) => {
                     </div>
                     <div className="col-12 mx-auto col-md-6 my-3 text-capitalize">
                       <p className="lead">{p.fields.descricao}</p>
+                       <Link href="/">
+                           <a className="btn btn-outline-warning my-2">Voltar<i class="fas fa-arrow-circle-left" aria-hidden="true"></i></a>
+                       </Link>
                       <div className="text-muted">
                           <hr/>
                           <Date dateString={p.sys.createdAt} />
