64007b92d37afacaae315629c760adc5e1192c22
IF <iisaquielfernandes@gmail.com>
2020-06-29 08:22:20 +0000

    Update Post.js

--- receita-web-site/components/Post.js
+++ receita-web-site/components/Post.js
@@ -13,15 +13,15 @@ const  Post = ({ posts, loading }) => {
     }
    
     return (
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 clearfix shadow-sm" itemtype ="http://schema.org/Receita">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 clearfix" itemtype ="http://schema.org/Receita">
             { posts.map((p) => (
-                <article key={p.sys.id} className="col mx-auto mb-3 px-sm-2">
+                <article key={p.sys.id} className="col mx-auto mb-3 px-sm-2 shadow-sm">
                     <div id={p.fields.categoria} className="card rounded-0 h-100 img-container" >
                         <img src={p.fields.image.fields.file.url} className="card-img-top img-fluid rounded-0" alt={p.fields.nome}/>
                         <div className="card-body p-2">
                             <h5 className="font-weight-lighter text-truncate my-3">{p.fields.nome}</h5>
                              <Link className="" href="/post/[id]" as={`/post/${p.sys.id}`}>
-                                <a className="btn btn-outline-warning mb-3">Ver Receita <i className="fa fa-cutlery" aria-hidden="true"></i></a>
+                                <a className="btn btn-outline-warning my-2">Ver Receita <i className="fa fa-cutlery" aria-hidden="true"></i></a>
                              </Link>
                         </div>
                     </div>
