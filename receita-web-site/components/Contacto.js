c3a8107cb7966d8c4644a4fd4283a4f351270d18
IF <iisaquielfernandes@gmail.com>
2020-06-29 16:27:54 +0000

    Update Contacto.js

--- receita-web-site/components/Contacto.js
+++ receita-web-site/components/Contacto.js
@@ -12,8 +12,8 @@ const Contacto = () => (
                     <strong>Address</strong><br /><i className="fas fa-map-marker-alt" /> Setúbal, 2810-226 Almada<br /><i className="fas fa-map-marker-alt" /> Lisboa, Portugal
                 </p>
                 <p className="col-sm-12 col-md-6 col-lg-4">
-                    <strong>Contact</strong><br /><i className="fas fa-mobile-alt mr-1" />935728467<i className="fas fa-phone-square-alt ml-2 mr-1" /> 945790354<br /><i className="fas fa-envelope-square mr-1" />
-                     chefsassyfernandescv@gmail.com
+                    <strong>Contact</strong><br /><i className="fas fa-mobile-alt mr-1" />935728467<br/><i className="fas fa-phone-square-alt ml-2 mr-1" /> 945790354
+                     
                 </p>
                 <p className="col-sm-12 col-md-12 col-lg-4">
                     <strong>Hours</strong><br /><i className="far fa-clock" /> Mon–Fri: 9:00AM–5:00PM<br /><i className="far fa-clock" /> Sat &amp; Sun: 11:00AM–3:00PM
