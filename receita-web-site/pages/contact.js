bca522d19942cd915e8f3b25d9912279c64b3c17
IF <iisaquielfernandes@gmail.com>
2020-06-30 09:52:49 +0100

    update

--- receita-web-site/pages/contact.js
+++ receita-web-site/pages/contact.js
@@ -8,36 +8,36 @@ const Contact = () => {
     <Container>
       <Head>Contatos</Head>
       <div className="container">
-      <section className="row py-5">
-        <article className="col-md-6 offset-md-3">
-          <h1 className="mb-3">Contato</h1>
-          <form action="https://formspree.io/mrgyarbl" method="POST">
-            <div className="form-group">
-              <input
-                type="text"
-                name="name"
-                placeholder="name"
-                className="form-control mb-1"
-              />
-              <input
-                type="email"
-                placeholder="email"
-                name="email"
-                className="form-control mb-1"
-              />
-              <textarea
-                name="message"
-                rows="5"
-                placeholder="message"
-                className="form-control"
-              ></textarea>
-            </div>
-            <button type="submit" className="btn btn-outline-success">
-              Enviar<i class="fas fa-paper-plane ml-1"></i>
-            </button>
-          </form>
-        </article>
-      </section>
+        <section className="row py-5">
+          <article className="col-md-4 offset-md-4">
+            <h1 className="mb-3">Contato</h1>
+            <form action="https://formspree.io/mrgyarbl" method="POST">
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  className="form-control mb-1"
+                />
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="form-control mb-1"
+                />
+                <textarea
+                  name="message"
+                  rows="6"
+                  placeholder="message"
+                  className="form-control"
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-outline-success">
+                Enviar<i class="fas fa-paper-plane ml-1"></i>
+              </button>
+            </form>
+          </article>
+        </section>
       </div>
     </Container>
   )
