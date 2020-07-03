dcd5bb5b4e3f7d91a5377d61051722ddee36b5c7
IF <iisaquielfernandes@gmail.com>
2020-07-03 10:29:50 +0100

    Update Footer.js

--- receita-web-site/components/Footer.js
+++ receita-web-site/components/Footer.js
@@ -3,14 +3,20 @@ const Footer = () => (
     <>
         {/* footer */}
         <footer className="footer-section mt-4">
-        <a className="btn btn-lg" href="https://instagram.com/chefsassyfnandes" target="_blank" role="button"><i className="fa fa-instagram" /></a>
-        <a className="btn btn-lg" href="https://facebook.com/chfsassy/" target="_blank" role="button"><i className="fa fa-facebook" /></a>
-        <a className="btn btn-lg" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" role="button"><i className="fa fa-youtube" /></a>
-        <p className="copyright mt-2 pb-0">
-            Copyright ©
-            All rights reserved <i className="ion-heart" aria-hidden="true" /> by Art
-            Gastronomic!
-        </p>
+            <a className="btn btn-lg" href="https://instagram.com/chefsassyfnandes" target="_blank" role="button"rel="noreferrer noopener">
+                <i className="fa fa-instagram" />
+            </a>
+            <a className="btn btn-lg" href="https://facebook.com/chfsassy/" target="_blank" role="button" rel="noreferrer noopener">
+                <i className="fa fa-facebook" />
+            </a>
+            <a className="btn btn-lg" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" role="button"rel="noreferrer noopener">
+                <i className="fa fa-youtube" />
+            </a>
+            <p className="copyright mt-2 pb-0">
+                Copyright ©
+                All rights reserved <i className="ion-heart" aria-hidden="true" /> by Art
+                Gastronomic!
+            </p>
         </footer>
     </>
 );
