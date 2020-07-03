cc9298cd877b77da7197640ea2e80edc8f1f2332
IF <iisaquielfernandes@gmail.com>
2020-07-03 11:02:40 +0100

    Update Footer.js

--- receita-web-site/components/Footer.js
+++ receita-web-site/components/Footer.js
@@ -3,13 +3,13 @@ const Footer = () => (
     <>
         {/* footer */}
         <footer className="footer-section mt-4">
-            <a className="btn btn-lg" href="https://instagram.com/chefsassyfnandes" target="_blank" role="button"rel="noreferrer noopener">
+            <a className="btn btn-lg" href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener">
                 <i className="fa fa-instagram" />
             </a>
-            <a className="btn btn-lg" href="https://facebook.com/chfsassy/" target="_blank" role="button" rel="noreferrer noopener">
+            <a className="btn btn-lg" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener">
                 <i className="fa fa-facebook" />
             </a>
-            <a className="btn btn-lg" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" role="button" rel="noreferrer noopener">
+            <a className="btn btn-lg" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noopener">
                 <i className="fa fa-youtube" />
             </a>
             <p className="copyright mt-2 pb-0">
