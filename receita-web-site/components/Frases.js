b365cc4c0bd42500db3fa3d343e74359900c3868
IF <iisaquielfernandes@gmail.com>
2020-07-05 11:27:16 +0000

    Update Frases.js

--- receita-web-site/components/Frases.js
+++ receita-web-site/components/Frases.js
@@ -1,8 +1,8 @@
 
 const Frases = () => (
 
-    <div className="col-11 col-lg-5 shadow-sm card rounded my-2 mx-auto">
-        <div className="card-body text-center py-5">
+    <div className="col-12 col-lg-5 shadow-sm card rounded my-2 mx-auto">
+        <div className="card-body message-body text-center py-5">
             <img src="img/logo/logotipo.jpg" className="img-fluid img-thumbnail img-logo mb-2" width={120} height={120} alt="logotipo chefsassyfnandes" />
             <h4 className="card-title text-center"><b>Sassy Fernandes</b></h4>
             <h6 className="card-subtitle mb-2 text-muted" />
