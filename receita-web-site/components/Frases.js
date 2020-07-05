60262e2473d561122d027b91494a96b0197d4031
IF <iisaquielfernandes@gmail.com>
2020-07-05 11:58:48 +0000

    Update Frases.js

--- receita-web-site/components/Frases.js
+++ receita-web-site/components/Frases.js
@@ -2,10 +2,10 @@
 const Frases = () => (
 
     <div className="message col-11 col-md-5 shadow-sm my-2">
-        <div className="message-body text-center py-5">
+        <div className="message-body py-2">
             <img src="img/logo/logotipo.jpg" className="img-fluid img-thumbnail img-logo mb-2" width={120} height={120} alt="logotipo chefsassyfnandes" />
-            <h4 className="card-title text-center"><b>Sassy Fernandes</b></h4>
-            <p className="card-text text-center text-secondary">"COZINHAR É COMO A VIDA.
+            <h4 className=""><b>- Sassy Fernandes</b></h4>
+            <p className="text-secondary">"COZINHAR É COMO A VIDA.
                         CASE COM UMA PESSOA QUE SAIBA COZINHAR...
                         O AMOR ACABA...A FOME NÃO."
             </p>
