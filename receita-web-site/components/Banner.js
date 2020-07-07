e61d5d8b15cb8c8012fc5ea37def0477a6800b45
IF <iisaquielfernandes@gmail.com>
2020-07-07 18:41:01 +0000

    Update Banner.js

--- receita-web-site/components/Banner.js
+++ receita-web-site/components/Banner.js
@@ -10,11 +10,11 @@ const Banner = () => (
                     <b className="text-center text-info">
                        Receitas Novas Todas As Semanas
                     </b>
-                    <div className="btn-group my-sm-3" role="group">
+                    <div className="btn-group my-3" role="group">
                        <a className="btn btn-danger p-button-raised p-button-danger p-button-raised text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="mr-2 fa fa-youtube-play" aria-hidden="true" />Inscreva-se</a>
                     </div>
                 </div>
-                <div className="col-sm-12 col-md-8 col-lg-8 my-sm-3">
+                <div className="col-sm-12 col-md-8 my-3">
                     <div className="float-right social-media-links">
                         <a className="btn btn-warning p-button-raised p-button-warning" href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"><i className="fa fa-instagram" /></a>
                         <a className="btn btn-info p-button-raised p-button-info ml-1" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener"><i className="fa fa-facebook" /></a>
