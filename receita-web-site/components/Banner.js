725312a9b4d0f6cd248ad572ee71b93730866904
IF <iisaquielfernandes@gmail.com>
2020-07-07 18:30:12 +0000

    Update Banner.js

--- receita-web-site/components/Banner.js
+++ receita-web-site/components/Banner.js
@@ -11,14 +11,14 @@ const Banner = () => (
                        Receitas Novas Todas As Semanas
                     </b>
                     <div className="btn-group" role="group">
-                       <a className="p-button-danger p-button-raised text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="mr-2 fa fa-youtube-play" aria-hidden="true" />Inscreva-se</a>
+                       <a className="btn btn-danger p-button-raised p-button-danger p-button-raised text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="mr-2 fa fa-youtube-play" aria-hidden="true" />Inscreva-se</a>
                     </div>
                 </div>
                 <div className="col-sm-12 col-md-8 col-lg-8">
                     <div className="float-right social-media-links">
-                        <a className="p-button-warning" href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"><i className="fa fa-instagram" /></a>
-                        <a className="p-button-info ml-1" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener"><i className="fa fa-facebook" /></a>
-                        <a className="p-button-danger ml-1" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="fa fa-youtube" /></a>
+                        <a className="btn btn-warning p-button-raised p-button-warning" href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"><i className="fa fa-instagram" /></a>
+                        <a className="btn btn-info p-button-raised p-button-info ml-1" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener"><i className="fa fa-facebook" /></a>
+                        <a className="btn btn-danger p-button-raised p-button-danger ml-1" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="fa fa-youtube" /></a>
                     </div>
                     <hr />
                 </div>
