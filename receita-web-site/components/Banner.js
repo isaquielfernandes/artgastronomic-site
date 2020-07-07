967a9c032ba5e03cd2e54811059fe36f10c2d589
IF <iisaquielfernandes@gmail.com>
2020-07-07 17:49:15 +0000

    Update Banner.js

--- receita-web-site/components/Banner.js
+++ receita-web-site/components/Banner.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Button } from 'primereact/button';
 
 const Banner = () => (
     <div className="social-media mt-0">
@@ -7,18 +8,16 @@ const Banner = () => (
                 <div className="col-sm-12 col-md-4 col-lg-4 text-center">
                     <h3 className="display-6 display-sm-12 text-center" style={{color: '#FD281A', opacity: 1}}>Chef Sassy Fernandes</h3>
                     <p className="text-center text-info"><b>Receitas Novas Todas As Semanas</b></p>
-                    <p className="lead">
+                     <div className="btn-group" role="group">
+                           <a className="btn p-button-danger text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="mr-2 fa fa-youtube-play" aria-hidden="true" /> Inscreva-se</a>
+                     </div>
                     </p>
-                    <div className="btn-group" role="group">
-                        <a className="btn btn-secondary border text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="mr-2 fa fa-youtube-play" aria-hidden="true" /> Inscreva-se</a>
-                    </div>
-                    <p />
                 </div>
                 <div className="col-sm-12 col-md-8 col-lg-8">
                     <div className="float-right social-media-links">
-                        <a className="btn btn-warning border " href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"><i className="fa fa-instagram" /></a>
-                        <a className="btn btn-info border  ml-1" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener"><i className="fa fa-facebook" /></a>
-                        <a className="btn btn-danger border ml-1" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="fa fa-youtube" /></a>
+                        <a className="btn p-button-warning " href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"><i className="fa fa-instagram" /></a>
+                        <a className="btn p-button-info ml-1" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener"><i className="fa fa-facebook" /></a>
+                        <a className="btn p-button-danger ml-1" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer"><i className="fa fa-youtube" /></a>
                     </div>
                     <hr />
                 </div>
