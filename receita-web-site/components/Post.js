fbd7edc53fa1d3ec7e01ce78ba46d82541495339
IF <iisaquielfernandes@gmail.com>
2020-07-12 15:15:17 +0100

    Update Post.js

--- receita-web-site/components/Post.js
+++ receita-web-site/components/Post.js
@@ -17,9 +17,9 @@ const  Post = ({ posts, loading }) => {
             { posts.map((p) => (
                 <article key={p.sys.id} className="col mx-auto mb-3 px-sm-2">
                     <div className="card rounded-0 h-100 img-container shadow-sm" >
-                        <img src={p.fields.image.fields.file.url} className="card-img-top img-fluid rounded-0" alt={p.fields.nome}/>
+                        <img src={p.fields.image.fields.file.url} className="card-img-top img-fluid rounded-0" alt=`img-${p.fields.nome}` />
                         <div className="card-body p-2">
-                            <h5 className="text-truncate my-3">{p.fields.nome}</h5>
+                            <h3 className="text-truncate my-3">{p.fields.nome}</h3>
                              
                         </div>
                         <div className="card-footer">
