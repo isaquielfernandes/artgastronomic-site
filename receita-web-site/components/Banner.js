8ff6813b827877494c97d844492aeaed15a6ec4c
IF <iisaquielfernandes@gmail.com>
2020-07-12 19:28:18 +0100

    Update Banner.js

--- receita-web-site/components/Banner.js
+++ receita-web-site/components/Banner.js
@@ -10,25 +10,25 @@ const Banner = () => (
                     <b className="text-center text-white bg-dark p-2 my-2">
                        Receitas Novas Todas As Semanas
                     </b>
-                    <div className="btn-group my-3">
-                       <a title="YouTube" className="btn btn-danger p-button-danger p-button-raised text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer noopener"><i className="mr-2 fa fa-youtube-play" />Inscreva-se</a>
+                    <div className="lead btn-group my-3">
+                       <a className="btn btn-danger p-button-danger p-button-raised text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer noopener"><i className="mr-2 fa fa-youtube-play" />Inscreva-se</a>
                     </div>
                 </div>
                 <div className="col-sm-12 col-md-8 my-3">
                     <div className="float-right social-media-links">
-                        <a title="Instagram" className="btn btn-warning p-button-raised p-button-warning" href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"> 
+                        <a className="btn btn-warning p-button-raised p-button-warning" href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"> 
                           <i className="fa fa-instagram" />
                           <span className="sr-only">
                             Instagram
                           </span>
                         </a>
-                        <a title="facebook" className="btn btn-info p-button-raised p-button-info ml-1" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener"> 
+                        <a className="btn btn-info p-button-raised p-button-info ml-1" href="https://facebook.com/chfsassy/" target="_blank" rel="noreferrer noopener"> 
                           <i className="fa fa-facebook" />
                           <span className="sr-only">
                             Facebook
                           </span>
                         </a>
-                        <a title="YouTube" className="btn btn-danger p-button-raised p-button-danger ml-1" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer" noopener>
+                        <a className="btn btn-danger p-button-raised p-button-danger ml-1" href="https://www.youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer" noopener>
                           <i className="fa fa-youtube" />
                           <span className="sr-only">
                             YouTube
