416493e06cf83d0a694d643a6fe6666919fe86d5
IF <iisaquielfernandes@gmail.com>
2020-07-14 23:09:10 +0100

    Update Banner.js

--- receita-web-site/components/Banner.js
+++ receita-web-site/components/Banner.js
@@ -7,14 +7,14 @@ const Banner = () => (
             <div className="row info-social jumbotron">
                 <div className="col-sm-12 col-md-4 col-lg-4 text-center">
                     <h3 className="display-6 display-sm-12 text-center" style={{color: '#FD281A'}}>Chef Sassy Fernandes</h3>
-                    <b className="text-center bg-dark p-2 my-2" style={{color: '#0A0A0A'}}>
+                    <b className="text-center bg-dark p-2 my-3" style={{color: '#0A0A0A'}}>
                        Receitas Novas Todas As Semanas
                     </b>
-                    <div className="lead btn-group my-3">
+                    <div className="lead my-3">
                        <a style={{color: '#0F0F0F'}} className="btn btn-danger p-button-danger p-button-raised text-uppercase" href="https://youtube.com/channel/UCybV8OZ7XZ9U30ESJ_r2UCg" target="_blank" rel="noreferrer noopener"><i className="mr-2 fa fa-youtube-play" />Inscreva-se</a>
                     </div>
                 </div>
-                <div className="col-sm-12 col-md-8 mt-4 mb-2">
+                <div className="col-sm-12 col-md-8 mt-3 mb-2">
                     <div className="float-right social-media-links">
                         <a className="btn link-media-social rounded-circle" href="https://instagram.com/chefsassyfnandes" target="_blank" rel="noreferrer noopener"> 
                           <i className="fab fa-instagram"></i>
