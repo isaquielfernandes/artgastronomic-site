e60bea2800a0941a20a5bfe6f4393064fe7e3cf2
IF <iisaquielfernandes@gmail.com>
2020-07-14 23:26:31 +0100

    Update Banner.js

--- receita-web-site/components/Banner.js
+++ receita-web-site/components/Banner.js
@@ -35,7 +35,6 @@ const Banner = () => (
                           </span>
                         </a>
                     </div>
-                    <hr />
                 </div>
             </div>
         </div>
