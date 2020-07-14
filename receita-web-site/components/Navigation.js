d0c240c5a880258e722b9341fe595dddf199692c
IF <iisaquielfernandes@gmail.com>
2020-07-14 23:37:06 +0100

    Update Navigation.js

--- receita-web-site/components/Navigation.js
+++ receita-web-site/components/Navigation.js
@@ -12,7 +12,7 @@ const Navigation = () => {
     <header>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
             <Link href="/">     
-                <a className="navbar-brand">
+                <a className="navbar-brand" style={{letterSpacing: '.7px'}}>
                     <Logo/>
                     ArtGastronomic
                 </a>
