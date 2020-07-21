16f743d23f8ac9fcf127761de4acd0931c03076a
IF <iisaquielfernandes@gmail.com>
2020-07-21 18:31:22 +0100

    Update Pagination.js

--- receita-web-site/components/Pagination.js
+++ receita-web-site/components/Pagination.js
@@ -1,9 +1,22 @@
+import {Pagination} from 'react-bootstrap'
 
+const Pagination = ({page, setPage, hasNextPage}) => {
 
-const Pagination = ({children}) => (
-    <div className="card-footer">
-        {children}
-    </div>
-)
+    const adjustPage = (amount) => {
+        setPage(prevPage => prevPage + amount)
+    }
+
+    return (
+        <Pagination>
+            {page !== 1 && <Pagination.Prev onClick={() => adjustPage(-1)} />}
+            {page !== 1 && <Pagination.Item onClick={() => setPage(1)}>1</Pagination.Item>}
+            {page > 2 && <Pagination.Ellipsis />}
+            {page > 2 && <Pagination.Item onClick={() => adjustPage(-1)}>{page - 1}</Pagination.Item>}
+            <Pagination.Item active>{page}</Pagination.Item>
+            {hasNextPage && <Pagination.Item onClick={() => adjustPage(1)}>{page + 1}</Pagination.Item>}
+            {hasNextPage && <Pagination.Next onClick={() => adjustPage(1)} />}
+        </Pagination>
+    );
+};
 
 export default Pagination;
